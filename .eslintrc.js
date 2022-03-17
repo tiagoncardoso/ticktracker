dccd7e5231352aebbca44cd3cb1851fb482e3708
Tiago Cardoso <tiago.mncardoso@gmail.com>
2022-03-17 09:50:32 -0300

    chore: Aplying lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,25 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/typescript/recommended'
-  ],
-  parserOptions: {
-    ecmaVersion: 2020
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
-}
+    root: true,
+    env: {
+        node: true,
+    },
+    'extends': [
+        'plugin:vue/vue3-essential',
+        'eslint:recommended',
+        '@vue/typescript/recommended',
+    ],
+    parserOptions: {
+        ecmaVersion: 2020,
+    },
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'indent': ['error', 4,],
+        'linebreak-style': ['error', 'unix',],
+        'quotes': ['warn', 'single',],
+        'semi': ['error', 'always',],
+
+        'comma-dangle': ['error', 'always',],
+        'no-cond-assign': ['error', 'always',],
+    },
+};
